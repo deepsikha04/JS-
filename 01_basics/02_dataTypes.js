3d34ec80a74946ab9d6ae3c175188543301a9b9a
deepsikha04 <dipsghimire5@gmail.com>
2024-12-05 09:34:12 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all js code to newer version
+// alert(3+3) // we are using node js, not browser
+
+let name ="Ram"
+let age = 20
+let isLoggedIn = false
+let state;
+
+console.log(typeof "Ram")
+console.log(typeof 20)
+console.log(typeof false)
+console.log(typeof null)
