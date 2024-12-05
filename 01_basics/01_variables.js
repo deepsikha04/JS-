c981a034254f07d37f165c4c2a9893158aba53c7
deepsikha04 <dipsghimire5@gmail.com>
2024-12-05 09:21:13 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "deepsikha@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ktm"
+
+
+//accountId = 2 not allowed
+
+accountEmail = "biswash@gmail.com"
+accountPassword = "abcdef123"
+accountCity = "Brt"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
