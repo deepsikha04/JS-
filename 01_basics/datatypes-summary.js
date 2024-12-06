55e810139684c19e217b7daa5280f53b3182130f
deepsikha04 <dipsghimire5@gmail.com>
2024-12-06 07:36:46 +0000

    Summary to datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+//2 types of datatypes: Primitive and Refernce(Non premitive)
+//Primitive (7 types)
+//string, number, boolean, null, undefined, symbol, BigInt
+//Reference(Non primitive)
+//Array, Objects, Functions
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = false
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 345678902345678n
