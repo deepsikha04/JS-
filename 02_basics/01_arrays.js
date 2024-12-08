76d204c4637655658bb9a0d91c6027046146cee7
deepsikha04 <dipsghimire5@gmail.com>
2024-12-08 09:37:22 +0000

    Array and its methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","naagraj"]
+const myArr2 = new Array(1,2,3,4)
+//console.log(myArr[2])
+
+//Array Methods
+//myArr.push(6)
+//myArr.push(8)
+//myArr.pop()
+//myArr.unshift(8)
+//myArr.shift()
+//console.log(myArr.includes(9))
+//console.log(myArr.indexOf(9))
+//console.log(myArr);
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof newArr);
+
+
+//slice and splice
