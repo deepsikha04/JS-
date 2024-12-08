3276ab0566ca868c28313ff4553d8f476ecf83a7
deepsikha04 <dipsghimire5@gmail.com>
2024-12-08 06:02:52 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name = "Deepsikha"
+const repoCount = 50
+ //console.log(name + repoCount +" Value");
+ //console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`);
+  const gameName = new String('Deepsikha')
+  const newString = gameName.slice(-8,4)
+  //console.log(newString);
+
+  const url = "https://deepsikha.com/deepbish%30ghimire"
+  //console.log(url.replace('%30','-'))
+  //console.log(url.includes('deepbish'))
+  console.log(gameName.split('-'));
