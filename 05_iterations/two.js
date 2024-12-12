2c9ec452e5a2f0014dd15bfd08f2389f75b3b715
deepsikha04 <dipsghimire5@gmail.com>
2024-12-12 01:31:25 +0000

    while do..while

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+let i = 0
+while(i<=10){
+    console.log(`Value of index is ${i}`)
+    i = i+2
+}
+
+let a1 = ["apple", "ball", "cat"]
+let a2 = 0
+while(a2 < a1.length ){
+    console.log(`value is ${a1[a2]}`)
+    a2++
+}
+
+let score = 11
+do{
+    console.log(`Score is ${score}`);
+    score++
+} while(score<=10);
