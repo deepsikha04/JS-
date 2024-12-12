c5faa6435ada3ce4d15a2cede4e7c000700a9cc9
deepsikha04 <dipsghimire5@gmail.com>
2024-12-12 05:55:13 +0000

    for in for objects

---
+++ 05_iterations/four.js
@@ -0,0 +1,14 @@
+const myObj = {
+    js : 'Javascript',
+    cpp : 'C++',
+    rb : 'Ruby',
+    swift: 'Swift by apple',
+}
+for (const key in myObj) {
+   //console.log(`${key} shortcut is for ${myObj[key]}`)
+}
+
+const programming = ["js","py","rb","java","cpp"]
+for (const key in programming) {
+    console.log(programming[key])
+    }
